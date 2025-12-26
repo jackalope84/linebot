f9116b7e6abc3786fe7b6ac37d48ae49a2c4d0b5
yoongjunyao <junyao84@hotmail.com>
2025-12-27 07:59:18 +0800

    Add files via upload

---
+++ ImageSendMessage/app.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+#載入LineBot所需要的套件
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import re
+app = Flask(__name__)
+
+# 必須放上自己的Channel Access Token
+line_bot_api = LineBotApi('6EnsTmbA8HylzZ2lBn2SvHBh4nDZsxLwAPFAOpTkkVhs/ZmA3b9c4dfFe+9b6JO/wxX3gqJJgGyPFCUok7JkDtyF1gUgex2lgih6i28nhOb+AOGdNbz9d7dsYQgCR++CydY4ajh2jeEtz1x+sSV1nQdB04t89/1O/w1cDnyilFU=')
+# 必須放上自己的Channel Secret
+handler = WebhookHandler('5102e18f461cfcf78a847a33a126ec54')
+
+line_bot_api.push_message('Uff248f1ad34ea853e4a785d4eb82799e', TextSendMessage(text='你可以開始了'))
+
+# 監聽所有來自 /callback 的 Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+
+    return 'OK'
+
+#訊息傳遞區塊
+##### 基本上程式編輯都在這個function #####
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = text=event.message.text
+    if re.match('告訴我秘密',message):
+        image_message = ImageSendMessage(
+            original_content_url='https://www.campus-studio.com/download/flag.jpg',
+            preview_image_url='https://www.campus-studio.com/download/101.jpg'
+        )
+        line_bot_api.reply_message(event.reply_token, image_message)
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(message))
+#主程式
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
